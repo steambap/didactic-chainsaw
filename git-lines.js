1e5723f6282a692e936eb97d10d29ec601c961ba
Weilin <934587911@qq.com>
2016-11-18 14:27:44 +0800

    fix calculation error

--- git-lines.js
+++ git-lines.js
@@ -8,20 +8,22 @@ const repoTable = {
 	'shiweilin.cn': 618,
 	'sublemon': 4145,
 	'query-element': 1679,
-	'lc2-tutorial': 2451,
+	'lc2-tutorial': 2974,
 	'lemon-cloud': 2997,
 	'electron-pref': 2305,
-	'probe-client': 8595,
+	'probe-client': 8939,
 	'probe-simple-report': 7419,
 	'svg-captcha': 2767,
 	'cloud-core': 6044,
 	'lemoncase2': 16673 + 4122,
 	'cloud-extension-public': 132,
 	'cloud-extension-task': 510,
-	'lemon-public': 2019
+	'lemon-public': 2019,
+	'warring-states': 6099,
+	'lc2-compiler': 5404
 };
 
-const total = 0;
+let total = 0;
 Object.keys(repoTable).forEach(repoName => {
 	total += repoTable[repoName];
 });
